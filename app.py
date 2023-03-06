360ffe60ca49c8f7e4eb7a2f6a260568e903ccf6
Pradip-Mane <manepradip18@gmail.com>
2023-03-07 01:28:04 +0530

    web app is ready

--- app.py
+++ app.py
@@ -2,8 +2,12 @@ import pickle
 from flask import Flask, request, app, jsonify, url_for, render_template
 import numpy as np
 import pandas as pd
+import os
 
 app=Flask(__name__)     # starting point of our app
+
+IMG_FOLDER=os.path.join('static','IMG')
+app.config['UPLOAD_FOLDER']=IMG_FOLDER
 
 ##Load the model
 regmodel=pickle.load(open('reg_adv_model.pkl', 'rb')) #rb
@@ -15,8 +19,10 @@ def home():
 @app.route('/predict', methods=['POST'])
 def predict():
     data=[float(x) for x in request.form.values()]               
-    output=regmodel.predict(np.array(data).reshape(1,-1))                   
-    return render_template("index.html", prediction_text="The Impact of advertisiment on Sales is {}".format(output[0])) 
+    output1=regmodel.predict(np.array(data).reshape(1,-1))
+                       
+    return render_template("index.html", prediction_text="The Impact of advertisiment on Sales is {}".format(output1[0])
+                           ) 
                    
     
 if __name__=="__main__":
